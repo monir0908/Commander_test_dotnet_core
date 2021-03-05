49c9522352161032ee732af1aa51613ec6ea3f2e
Monir Hossain <monirhossain0908@gmail.com>
2021-03-05 23:35:01 +0600

    signalr working from both sides

--- Startup.cs
+++ Startup.cs
@@ -26,8 +26,20 @@ namespace Commander
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddControllers();
+            services.AddSignalR();
+
+
+            services.AddCors(option =>
+            {
+                option.AddPolicy("CorsPolicy", builder =>
+                         builder.WithOrigins("*", "http://localhost:4200")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Commander", Version = "v1" });
@@ -37,6 +49,8 @@ namespace Commander
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -44,15 +58,23 @@ namespace Commander
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Commander v1"));
             }
 
+
+            
+            
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors("CorsPolicy");
+            
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<SignalHub>("/pushNotification");
             });
         }
     }
